7b255d934321120b231dd2d844d64a436458d2a5
Дмитрий <tartarianec@gmail.com>
2016-10-12 19:24:09 +0700

    Просто черепашка, просто живёт тут

--- Turtle Training/Program.cs
+++ Turtle Training/Program.cs
@@ -14,18 +14,38 @@ namespace Turtle_Training
         {
             GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
             Turtle.PenUp();
-
-            GraphicsWindow.BrushColor = "Green";
+            GraphicsWindow.BrushColor = "Green";            
             var eat = Shapes.AddRectangle(10, 10);
-            Shapes.Move(eat, 200, 200);
-            while (true)
+            int x, y, life=3;
+            int score =0;
+            Random rand = new Random();
+            x = rand.Next(0, GraphicsWindow.Width);
+            y = rand.Next(0, GraphicsWindow.Height);
+            Shapes.Move(eat, x, y);            
+            while (life>0)
             {
                 Turtle.Move(1);
-            }
-
+                if (Turtle.X >= x && Turtle.X <= x + 10 && Turtle.Y >= y && Turtle.Y <= y + 10)
+                {
+                    x = rand.Next(0, GraphicsWindow.Width);
+                    y = rand.Next(0, GraphicsWindow.Height);
+                    Shapes.Move(eat, x, y);
+                    Turtle.Speed++;
+                    score++;
+                    
+                    GraphicsWindow.DrawText(GraphicsWindow.Width - 100, GraphicsWindow.Height - 100, score);
+                }
+                else if (Turtle.X < 0 || Turtle.X>GraphicsWindow.Width || Turtle.Y < 0 || Turtle.Y > GraphicsWindow.Height) 
+                {
+                    Turtle.X = GraphicsWindow.Width / 2;
+                    Turtle.Y = GraphicsWindow.Height / 2;
+                    life--;
+                }
+            }            
         }
 
-        private static void GraphicsWindow_KeyDown()
+        //Обработка нажатий клавиш
+        private static void GraphicsWindow_KeyDown()        
         {
             if (GraphicsWindow.LastKey == "Up")
             {
@@ -46,9 +66,8 @@ namespace Turtle_Training
             else if (GraphicsWindow.LastKey == "Escape")
             {
                 Environment.Exit(0);              
-            }
-          
+            }       
                
-        }
+        } 
     }
 }
