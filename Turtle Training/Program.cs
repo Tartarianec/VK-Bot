4d27b4907cb5f19fc07d93bc3478c0908bc3015d
Дмитрий <tartarianec@gmail.com>
2016-10-12 13:05:39 +0700

    Черепашка теперь двигается с помощью стрелок

--- Turtle Training/Program.cs
+++ Turtle Training/Program.cs
@@ -11,7 +11,32 @@ namespace Turtle_Training
     {
         static void Main(string[] args)
         {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            while (true)
+            {
+                Turtle.Move(10);
+            }
+        }
 
+        private static void GraphicsWindow_KeyDown()
+        {
+            if (GraphicsWindow.LastKey == "Up")
+            {
+                Turtle.Angle = 0;
+            }
+            else if(GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+            else if(GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+            else if(GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
+               
         }
     }
 }
